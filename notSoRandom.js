6134122af37c18a7cb9c135366a924b95599ec2d
manuelASC5 <manuelnajera1119@gmail.com>
2018-07-12 12:02:44 -0400

    work

--- notSoRandom.js
+++ notSoRandom.js
@@ -4,3 +4,12 @@ randomNumber = Math.floor(randomNumber);
 console.log(randomNumber);
 }
 number(90);
+randomLetter - takes 1 string argument and console.logs a random letter from the given string
+randomLetter(“blah”); // “b”
+randomLetter(“abcdefg”); // “g”
+randomLetter(“z”); // “z”
+myGuy - takes 1 string argument and console.logs the same string with “, my guy.” at the end of it
+myGuy(“Take it easy”); // “Take it easy, my guy”
+string_N_times - takes 2 arguments, a string and a number, and console.logs the string as many times as the given number
+hypotenuse - takes 2 number arguments (corresponding to the perpendicular sides of a right triangle) and console.logs the length of the 3rd side, the hypotenuse.
+NOTE: don’t worry about the math! Here’s the equation and some links to helpful functions in JavaScript
